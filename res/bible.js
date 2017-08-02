76e975d899b67d47935a5d91602e57afcab1684d
leptune <leptune@sina.com>
2017-08-02 12:48:04 +0800

    [deploy]完成自定义菜单

--- res/bible.js
+++ res/bible.js
@@ -10,7 +10,7 @@ $(document).ready(function() {
 		}
 		$('.bible-body-old').append('<li><h4>'+obj.abbr+'</h4><h5>'+name+'</h5></li>')
 	}
-	for (var i = 0; i < 4; i++) {
+	for (var i = 0; i < 5; i++) {
 		$('.bible-body-old').append('<li class="no"><h4>&nbsp;</h4><h5>&nbsp;</h5></li>')
 	}
 	for (name in bible_data) {
@@ -24,12 +24,12 @@ $(document).ready(function() {
 		}
 		$('.bible-body-new').append('<li><h4>'+obj.abbr+'</h4><h5'+style+'>'+name+'</h5></li>')
 	}
-	for (var i = 0; i < 4; i++) {
+	for (var i = 0; i < 5; i++) {
 		$('.bible-body-new').append('<li class="no"><h4>&nbsp;</h4><h5>&nbsp;</h5></li>')
 	}
 	$('.bible-body-old li:first').addClass('active');
 
-	$("body").niceScroll();
+	$('body').niceScroll();
 	$(document).on('click', ".bible-header-juan-zhang li", function(event) {
 		$(".bible-header-juan-zhang li.active").removeClass('active');
 		$(this).addClass('active');
@@ -41,6 +41,15 @@ $(document).ready(function() {
 			for (var i = 1; i <= bible_data[name].count; i++) {
 				$('.bible-body-zhang').append('<li><h5>'+i+'</h5></li>');
 			}
+			var tmpCount = bible_data[name].count%5;
+			if (!tmpCount) {
+				tmpCount = 5;
+			} else {
+				tmpCount = 10 - tmpCount;
+			}
+			for (var i = 0; i < tmpCount; i++) {
+				$('.bible-body-zhang').append('<li class="no"><h5>&nbsp;</h5></li>')
+			}
 			var index = $('.bible-body-juan li.active').data('zhang');
 			if (index) {
 				$('.bible-body-zhang li:eq('+(+index-1)+')').addClass('active');
@@ -51,12 +60,14 @@ $(document).ready(function() {
 		}
 		$('.bible-foot-type').toggle();
 		$('.bible-foot-tips').toggle();
+		$('body').getNiceScroll().resize();
 	});
 	$(document).on('click', ".bible-foot-type li", function(event) {
 		$(".bible-foot-type li.active").removeClass('active');
 		$(this).addClass('active');
 		$('.bible-body-juan ul').hide();
 		$($(this).data('class')).show();
+		$('body').getNiceScroll().resize();
 	});
 	$(document).on('click', ".bible-body-juan li", function(event) {
 		if ($(this).is('.no')) {
@@ -65,14 +76,19 @@ $(document).ready(function() {
 		$(".bible-body-juan li.active").removeClass('active');
 		$(this).addClass('active');
 		$('.bible-header-zhang').click();
+		$('body').getNiceScroll().resize();
 	});
 	$(document).on('click', ".bible-body-zhang li", function(event) {
+		if ($(this).is('.no')) {
+			return false;
+		}
 		$(".bible-body-zhang li.active").removeClass('active');
 		$(this).addClass('active');
 		var name = $('.bible-body-juan li.active h5').text();
 		var index = $(this).find('h5').text();
 		$('.bible-body-juan li.active').data('zhang', index);
 		$('.bible-foot-tips').text('('+name+'第'+index+'章)');
+		$('body').getNiceScroll().resize();
 	    player[0].source({
 		    type: 'audio',
 		    sources: 'http://otzkd2ox3.bkt.clouddn.com/'+name+'第'+index+'章.mp3'
